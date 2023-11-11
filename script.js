c0d62b9c15fb18995851324fa88115c185a12951
Pranshu Chaudhary <wittywit@users.noreply.github.com>
2023-11-12 03:10:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,236 +1,76 @@
-var log = console.log.bind(console),
-    keyData = document.getElementById('key_data'),
-    midi;
-var AudioContext = AudioContext || webkitAudioContext; // for ios/safari
-var context = new AudioContext();
-var btnBox = document.getElementById('content'),
-    btn = document.getElementsByClassName('button');
-var data, cmd, channel, type, note, velocity;
+gsap.registerPlugin(CustomEase);
 
-// request MIDI access
-if (navigator.requestMIDIAccess) {
-    navigator.requestMIDIAccess({
-        sysex: false
-    }).then(onMIDISuccess, onMIDIFailure);
-} else {
-    alert("No MIDI support in your browser.");
+const customEaseIn = CustomEase.create('custom-ease-in', '0.52, 0.00, 0.48, 1.00');
+const fourtyFrames = 1.3333333;
+const fiftyFrames = 1.66666;
+const twoFrames = 0.666666;
+const fourFrames = 0.133333;
+const sixFrames = 0.2;
+
+const video = document.querySelector('.hero-video');
+const header = document.querySelector('.header');
+const book = document.querySelector('.first-desc span');
+const open = document.querySelector('.second-desc span');
+const copy = document.querySelector('.copyright span');
+const scrollToRows = document.querySelectorAll('.scroll-to .scroll-to__row span');
+const btnCircle = document.querySelector('.book-btn__circle');
+const btnText = document.querySelector('.btn-text span');
+const eve = document.querySelector('#eve span');
+const ry = document.querySelector('#ry span');
+const fo = document.querySelector('#fo span');
+const ssil = document.querySelector('#ssil span');
+const tells = document.querySelector('#tells span');
+const a = document.querySelector('#a span');
+const st = document.querySelector('#st span');
+const ory = document.querySelector('#ory span');
+
+const showElements = () => {
+  const timeline = gsap.timeline();
+  timeline
+        .fromTo(btnCircle, { autoAlpha: 0 }, { autoAlpha: 1, duration: fourtyFrames, ease: customEaseIn}, 0)
+        .fromTo(btnCircle, { scale: 0.417 }, { scale: 1, duration: fourtyFrames, ease: customEaseIn}, 0)
+        .fromTo(header, {y: '-0.5rem'}, {y: '0rem', duration: fourtyFrames, ease: customEaseIn}, 0)
+        .fromTo(eve, {x: '2.7rem'}, { x: '0rem', duration: fiftyFrames, ease: customEaseIn}, 0)
+        .fromTo(book, {y: '0.5rem'}, {y: '0rem', duration: fourtyFrames, ease: customEaseIn}, twoFrames)
+        .fromTo(fo, {x: '2.1rem'}, { x: '0rem', duration: fiftyFrames, ease: customEaseIn}, twoFrames)
+        .fromTo(a, {x: '-1.2rem'}, { x: '0rem', duration: fiftyFrames, ease: customEaseIn}, twoFrames)
+        .fromTo(ory, {x: '-3.2rem'}, { x: '0rem', duration: fiftyFrames, ease: customEaseIn}, twoFrames)
+        .fromTo(open, {y: '0.3rem'}, {y: '0rem', duration: fourtyFrames, ease: customEaseIn}, fourFrames)
+        .fromTo(btnText, {y: '0.4rem'}, {y: '0rem', duration: fourtyFrames, ease: customEaseIn}, fourFrames)
+        .fromTo(ry, {x: '-2rem'}, { x: '0rem', duration: fiftyFrames, ease: customEaseIn}, fourFrames)
+        .fromTo(ssil, {x: '-3.1rem'}, { x: '0rem', duration: fiftyFrames, ease: customEaseIn}, fourFrames)
+        .fromTo(tells, {x: '4.3rem'}, { x: '0rem', duration: fiftyFrames, ease: customEaseIn}, fourFrames)
+        .fromTo(st, {x: '1.9rem'}, { x: '0rem', duration: fiftyFrames, ease: customEaseIn}, fourFrames)
+        .fromTo(copy, {y: '0.4rem'}, {y: '0rem', duration: fourtyFrames, ease: customEaseIn}, sixFrames)
+        .fromTo(scrollToRows, {y: '0.5rem'}, {y: '0rem', duration: fourtyFrames, ease: customEaseIn}, sixFrames);
+  
+  return timeline;
 }
 
-// add event listeners
-document.addEventListener('keydown', keyController);
-document.addEventListener('keyup', keyController);
-for (var i = 0; i < btn.length; i++) {
-    btn[i].addEventListener('mousedown', clickPlayOn);
-    btn[i].addEventListener('mouseup', clickPlayOff);
-}
-// prepare audio files
-for (var i = 0; i < btn.length; i++) {
-    addAudioProperties(btn[i]);
-}
-// this maps the MIDI key value (59 - 69) to our samples
-var sampleMap = {
-    key59: 1,
-	key60: 2,
-    key61: 3,
-    key62: 4,
-    key64: 5,
-    key66: 6,
-	key68: 7,
-	key70: 8,
-	key72: 9
-};
-// user interaction, mouse click
-function clickPlayOn(e) {
-    e.target.classList.add('active');
-    e.target.play();
+const hideElements = () => {
+  const timeline = gsap.timeline();
+  
+  timeline
+        .fromTo(copy, {y: '0rem'}, {y: '0.4rem', duration: fourtyFrames, ease: customEaseIn}, 0)
+        .fromTo(scrollToRows, {y: '0rem'}, {y: '0.5rem', duration: fourtyFrames, ease: customEaseIn}, 0)
+        .fromTo(open, {y: '0rem'}, {y: '0.3rem', duration: fourtyFrames, ease: customEaseIn}, twoFrames)
+        .fromTo(btnText, {y: '0rem'}, {y: '0.4rem', duration: fourtyFrames, ease: customEaseIn}, twoFrames)
+        .fromTo(ry, {x: '0rem'}, { x: '-2rem', duration: fiftyFrames, ease: customEaseIn}, twoFrames)
+        .fromTo(ssil, {x: '0rem'}, { x: '-3.1rem', duration: fiftyFrames, ease: customEaseIn}, twoFrames)
+        .fromTo(tells, {x: '0rem'}, { x: '4.3rem', duration: fiftyFrames, ease: customEaseIn}, twoFrames)
+        .fromTo(st, {x: '0rem'}, { x: '1.9rem', duration: fiftyFrames, ease: customEaseIn}, twoFrames)
+        .fromTo(book, {y: '0rem'}, {y: '0.5rem', duration: fourtyFrames, ease: customEaseIn}, fourFrames)
+        .fromTo(fo, {x: '0rem'}, { x: '2.1rem', duration: fiftyFrames, ease: customEaseIn}, fourFrames)
+        .fromTo(a, {x: '0rem'}, { x: '-1.2rem', duration: fiftyFrames, ease: customEaseIn}, fourFrames)
+        .fromTo(ory, {x: '0rem'}, { x: '-3.2rem', duration: fiftyFrames, ease: customEaseIn}, fourFrames)
+        .fromTo(btnCircle, { autoAlpha: 1 }, { autoAlpha: 0, duration: fourtyFrames, ease: customEaseIn}, sixFrames)
+        .fromTo(btnCircle, { scale: 1 }, { scale: 0.417, duration: fourtyFrames, ease: customEaseIn}, sixFrames)
+        .fromTo(header, {y: '0rem'}, {y: '-100%', duration: fourtyFrames, ease: customEaseIn}, sixFrames)
+        .fromTo(eve, {x: '0rem'}, { x: '2.7rem', duration: fiftyFrames, ease: customEaseIn}, sixFrames);
+  
+  return timeline;
 }
 
-function clickPlayOff(e) {
-    e.target.classList.remove('active');
-}
-// qwerty keyboard controls. [q,w,e,r,t]
-function keyController(e) {
-    if (e.type == "keydown") {
-        switch (e.keyCode) {
-            case 81:
-                btn[0].classList.add('active');
-                btn[0].play();
-                break;
-            case 87:
-                btn[1].classList.add('active');
-                btn[1].play();
-                break;
-            case 69:
-                btn[2].classList.add('active');
-                btn[2].play();
-                break;
-            case 82:
-                btn[3].classList.add('active');
-                btn[3].play();
-                break;
-            case 84:
-                btn[4].classList.add('active');
-                btn[4].play();
-                break;
-            default:
-                //console.log(e);
-        }
-    } else if (e.type == "keyup") {
-        switch (e.keyCode) {
-            case 81:
-                btn[0].classList.remove('active');
-                break;
-            case 87:
-                btn[1].classList.remove('active');
-                break;
-            case 69:
-                btn[2].classList.remove('active');
-                break;
-            case 82:
-                btn[3].classList.remove('active');
-                break;
-            case 84:
-                btn[4].classList.remove('active');
-                break;
-            default:
-                //console.log(e.keyCode);
-        }
-    }
-}
-
-// midi functions
-function onMIDISuccess(midiAccess) {
-    midi = midiAccess;
-    var inputs = midi.inputs.values();
-    // loop through all inputs
-    for (var input = inputs.next(); input && !input.done; input = inputs.next()) {
-        // listen for midi messages
-        input.value.onmidimessage = onMIDIMessage;
-        // this just lists our inputs in the console
-        listInputs(input);
-    }
-    // listen for connect/disconnect message
-    midi.onstatechange = onStateChange;
-}
-
-function onMIDIMessage(event) {
-    data = event.data,
-    cmd = data[0] >> 4,
-    channel = data[0] & 0xf,
-    type = data[0] & 0xf0, // channel agnostic message type. Thanks, Phil Burk.
-    note = data[1],
-    velocity = data[2];
-    // with pressure and tilt off
-    // note off: 128, cmd: 8 
-    // note on: 144, cmd: 9
-    // pressure / tilt on
-    // pressure: 176, cmd 11: 
-    // bend: 224, cmd: 14
-
-    switch (type) {
-        case 144: // noteOn message 
-             noteOn(note, velocity);
-             break;
-        case 128: // noteOff message 
-            noteOff(note, velocity);
-            break;
-    }
-
-    //console.log('data', data, 'cmd', cmd, 'channel', channel);
-    logger(keyData, 'key data', data);
-}
-
-function onStateChange(event) {
-    var port = event.port,
-        state = port.state,
-        name = port.name,
-        type = port.type;
-    if (type == "input") console.log("name", name, "port", port, "state", state);
-}
-
-function listInputs(inputs) {
-    var input = inputs.value;
-    log("Input port : [ type:'" + input.type + "' id: '" + input.id +
-        "' manufacturer: '" + input.manufacturer + "' name: '" + input.name +
-        "' version: '" + input.version + "']");
-}
-
-function noteOn(midiNote, velocity) {
-    player(midiNote, velocity);
-}
-
-function noteOff(midiNote, velocity) {
-    player(midiNote, velocity);
-}
-
-function player(note, velocity) {
-    var sample = sampleMap['key' + note];
-    if (sample) {
-        if (type == (0x80 & 0xf0) || velocity == 0) { //QuNexus always returns 144
-            btn[sample - 1].classList.remove('active');
-            return;
-        }
-        btn[sample - 1].classList.add('active');
-        btn[sample - 1].play(velocity);
-    }
-}
-
-function onMIDIFailure(e) {
-    log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + e);
-}
-
-// audio functions
-// We'll go over these in detail in future posts
-function loadAudio(object, url) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function () {
-        context.decodeAudioData(request.response, function (buffer) {
-            object.buffer = buffer;
-        });
-    }
-    request.send();
-}
-
-function addAudioProperties(object) {
-    object.name = object.id;
-    object.source = object.dataset.sound;
-    loadAudio(object, object.source);
-    object.play = function (volume) {
-        var s = context.createBufferSource();
-        var g = context.createGain();
-        var v;
-        s.buffer = object.buffer;
-        s.playbackRate.value = randomRange(0.5, 2);
-        if (volume) {
-            v = rangeMap(volume, 1, 127, 0.2, 2);
-            s.connect(g);
-            g.gain.value = v * v;
-            g.connect(context.destination);
-        } else {
-            s.connect(context.destination);
-        }
-
-        s.start();
-        object.s = s;
-    }
-}
-
-// utility functions
-function randomRange(min, max) {
-    return Math.random() * (max + min) + min;
-}
-
-function rangeMap(x, a1, a2, b1, b2) {
-    return ((x - a1) / (a2 - a1)) * (b2 - b1) + b1;
-}
-
-function frequencyFromNoteNumber(note) {
-    return 440 * Math.pow(2, (note - 69) / 12);
-}
-
-function logger(container, label, data) {
-    messages = label + " [channel: " + (data[0] & 0xf) + ", cmd: " + (data[0] >> 4) + ", type: " + (data[0] & 0xf0) + " , note: " + data[1] + " , velocity: " + data[2] + "]";
-    container.textContent = messages;
-}
+document.addEventListener('DOMContentLoaded', () => {
+  showElements();
+});
